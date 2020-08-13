3b887649a10fa72d6b367f7867f29d9805bbadb1
Xavier Bouclet <xavier.bouclet@bnc.ca>
2020-08-13 14:10:37 -0400

    Token is now in variable

--- src/main/kotlin/ca/mikrethor/latestversion/Main.kt
+++ src/main/kotlin/ca/mikrethor/latestversion/Main.kt
@@ -1,15 +1,16 @@
 package ca.mikrethor.latestversion
 
-fun main() {
+fun main(args: Array<String>) {
+    val token=args[0]
     val springBootClient = TagClientBuilder.client("https://api.github.com/repos/spring-projects/spring-boot/tags")
-    val springBootTags = springBootClient.findAll()
+    val springBootTags = springBootClient.findAll(token)
     val springBootTag = springBootTags.first { it.name.contains("RELEASE") }
     println(springBootTag.name.removePrefix("v"))
 
-    val jibClient = TagClientBuilder.client("https://api.github.com/repos/GoogleContainerTools/jib/tags")
-    val jibTags = jibClient.findAll()
-    val jibTag = jibTags.first { it.name.contains("-maven") }
-    println(jibTag.name.removePrefix("v").removeSuffix("-maven"))
+//    val jibClient = TagClientBuilder.client("https://api.github.com/repos/GoogleContainerTools/jib/tags")
+//    val jibTags = jibClient.findAll()
+//    val jibTag = jibTags.first { it.name.contains("-maven") }
+//    println(jibTag.name.removePrefix("v").removeSuffix("-maven"))
 
 
 }
--- src/main/kotlin/ca/mikrethor/latestversion/TagClient.kt
+++ src/main/kotlin/ca/mikrethor/latestversion/TagClient.kt
@@ -2,11 +2,12 @@ package ca.mikrethor.latestversion
 
 import ca.mikrethor.latestversion.model.Tag
 import feign.Headers
+import feign.Param
 import feign.RequestLine
 
 interface TagClient {
 
     @RequestLine("GET")
-    @Headers("Authorization: token ADD YOUR GITHUB TOKEN HERE !!!!")
-    fun findAll():List<Tag>
+    @Headers("Authorization: token {token}")
+    fun findAll(@Param("token") token:String):List<Tag>
 }
